8bd0d5b9ff8eed54e962727b256a3b5e2038ab2a
Isaac-David-Vargas-Marenco <isaac.vargas.marenco@academiacuatropuntocero.com>
2022-05-20 19:46:23 -0600

    version 1.2

--- js/alert.js
+++ js/alert.js
@@ -1,9 +1,9 @@
-var form_tag = document.getElementById("form_medtronic");
+/*var form_tag = document.getElementById("form_medtronic");
 
 form_tag.addEventListener("submit", function(event){ 
  event.preventDefault();
 
- var name = document.getElementById("name_medtronic").value;
+ var name_med = document.getElementById("name_medtronic").value;
  var number = document.getElementById("telephone_medtronic").value;
  var email = document.getElementById("email_medtronic").value;
  var date = document.getElementById("date_medtronic").value;
@@ -11,5 +11,33 @@ form_tag.addEventListener("submit", function(event){
 
 
 
-alert("your name : "+name+" your number ; "+number+" your email : "+email+" your date : "+date+" your menssage : "+message);
-})
+//alert("your name : "+name+" your number ; "+number+" your email : "+email+" your date : "+date+" your menssage : "+message);
+})*/
+
+var submit_button = document.getElementById("submit-button")
+
+submit_button.onclick = function(event){
+  event.preventDefault();
+  var name_med = document.getElementById("name_medtronic").value;
+  var number_med = document.getElementById("telephone_medtronic").value;
+  var email_med = document.getElementById("email_medtronic").value;
+  var date_med = document.getElementById("date_medtronic").value;
+  var text_med = document.getElementById("texterea_medtronic").value;
+
+
+Swal.fire({
+    title: '<strong> your information is</strong>',
+    icon: 'information',
+    html:
+      ' inserted name value is : '+name_med+ ' inserted telephone value is : '+ number_med+ ' inserted email value is : '+email_med+' inserted date value is : '+date_med+' inserted message is : '+text_med,
+    showCloseButton: true,
+    showCancelButton: false,
+    focusConfirm: false,
+    confirmButtonText:
+      '<i class="fa fa-thumbs-up"></i> Great!',
+    confirmButtonAriaLabel: 'Thumbs up, great!',
+    cancelButtonText:
+      '<i class="fa fa-thumbs-down"></i>',
+    cancelButtonAriaLabel: 'Thumbs down'
+  })
+}//
