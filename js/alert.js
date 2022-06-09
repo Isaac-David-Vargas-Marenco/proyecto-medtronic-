d53c7d1d9ac4f925855c22311197f591c5553e1f
Isaac-David-Vargas-Marenco <isaac.vargas.marenco@academiacuatropuntocero.com>
2022-06-08 19:14:48 -0600

    version 1.8

--- js/alert.js
+++ js/alert.js
@@ -1,16 +1,11 @@
 /*var form_tag = document.getElementById("form_medtronic");
-
 form_tag.addEventListener("submit", function(event){ 
  event.preventDefault();
-
  var name_med = document.getElementById("name_medtronic").value;
  var number = document.getElementById("telephone_medtronic").value;
  var email = document.getElementById("email_medtronic").value;
  var date = document.getElementById("date_medtronic").value;
  var message = document.getElementById("texterea_medtronic").value;
-
-
-
 //alert("your name : "+name+" your number ; "+number+" your email : "+email+" your date : "+date+" your menssage : "+message);
 })*/
 
@@ -57,4 +52,4 @@ Swal.fire({
       '<i class="fa fa-thumbs-down"></i>',
     cancelButtonAriaLabel: 'Thumbs down'
   })
-}//
+}
